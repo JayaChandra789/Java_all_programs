9823cca058cf030554b0733c681aa703ba0386fd
JayaChandra789 <bagamjaya789@gmail.com>
2024-08-16 18:39:34 +0530

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,46 +1,71 @@
 package oopsExamples;
 import java.util.Scanner;
-//Define the superclass Car
-class Cars 
+
+class cars //Define the superclass Car
 {
-	String carBrand;
-	String carType;
-	String carFuelType;
-class Car extends Cars	
-{
-	public void inputCar() 
+	String carType,carFuelType,carVersion,selection,book; //Variables to be used 
+	Scanner sc = new Scanner(System.in); //Scanner
+	public void CarBMW_display()  //Method display list
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter car Brand like MAhindra: ");
-		carBrand = sc.nextLine();
-		System.out.print("Enter car Type like SUV,XUV etc.. : ");
-		carType = sc.nextLine();
-		System.out.print("Enter car Fuel Type like petrol,Diesel : ");
-		carFuelType = sc.nextLine();
-		System.out.print("The entered brand is :" + carBrand + "Body type is:" + carType +
-				"Car Fuel Type is: " + carFuelType );
+		System.out.println("Welcome to BMW ShowRoom ");
+		System.out.println("Our cars are BMW 7 series");
+		System.out.println("Our cars are BMW i8 Roadster");
+		System.out.println("Our cars are BMW M series are availaible in our Showroom");
+		System.out.println("Which car you would Like to select Enter your Specifications For car Booking:");	
 	}
-	public void displayCar() 
+	public void CarBooking() //Booking Method & selections
 	{
-		System.out.print("As you entered details "
-				+ "we have Suitable "
-				+ "car for you they are: Xuv300,XUV500 AND XUV 700 As per your mentioned specifications");
+		System.out.print("Please select Type:");
+		carType = sc.nextLine();
+		System.out.print("Please select FuelType:");
+		carFuelType = sc.nextLine();
+		System.out.print("Please select carVersion('Sports or Street Fighter'):");
+		carVersion = sc.nextLine();
+		if (carType.equalsIgnoreCase("sedan") && carFuelType.equalsIgnoreCase("petrol") 
+				&& carVersion.equalsIgnoreCase("sports"))
+		{
+			System.out.println("Nice selection " +carType+","+carFuelType+","+carVersion );
+		}
+		else if(carType.equalsIgnoreCase("suv") && carFuelType.equalsIgnoreCase("petrol") 
+				&& carVersion.equalsIgnoreCase("Sports"))
+		{
+			System.out.println("Nice selection " +carType+","+carFuelType+","+carVersion );
+		}
+		else 
+		{
+			System.out.println("Enter Valid Option");
+		}
+		selection = carType + " "+carFuelType + " "+carVersion; //Storing all variables in one variable
 	}
 }
-//Define the subclass Mahindra, inheriting from Car
-
-//Main class to test the functionality
-public class  Carmain
+class Carcase extends cars	 //DERIVED CLASS use super KEYWORD FOR ACCEESING DERIVED CLASS VARAIBLES NAD CONTENT INIT
+{
+	public void Order() 
+	{
+		System.out.print("Press yes To Book car: ");
+		super.book = super.sc.nextLine();
+		if (book.equalsIgnoreCase("yes")) //equalsIgnoreCase(" ") for matching String like ==
+		{
+			System.out.println("Than you Choosing BMW " +super.selection +" Have nice day :)  ");
+			System.out.println("Your order will be delivered Within One month");
+		}
+		else if (book.equalsIgnoreCase("no")) 
+		{
+			System.out.println("Order Not Proceeded,Hope you find best In Our BMW Next time" );  
+		}
+		else 
+		{
+			System.out.println("Invalid option ");
+		}
+	}
+}
+public class Car //Main class
 {
 	public static void main(String[] args) 
 	{
-		// Create an instance of Mahindra
-		Carmain mahindraVehicle = new Carmain();     
-		// Call methods from both the Car class and Mahindra class
-		mahindraVehicle.inputCar();
-		mahindraVehicle.displayCar();
-		
+		Carcase carobj = new Carcase(); //Car object for method calling
+		carobj.CarBMW_display();
+		carobj.CarBooking();
+		carobj.Order();
 	}
 }
-}
-
