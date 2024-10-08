f845f577041ac5ff34bc9a9ca7a5cad8039db311
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-08 10:09:18 +0530

    Update Array_names.java

--- Array_names.java
+++ Array_names.java
@@ -14,10 +14,11 @@ public class Array_names
 	{
 		System.out.print("Enter name to print list: ");
 		my_list[len] = sc.nextLine();  //here always use **array_name[varaible_name of length]**
+		System.out.print(my_list[len] + "+");
 	}
 	System.out.print("Entered 3 names are: "); //prints message
 	
-	for (len=0;len < my_list.length;len++) //loop for printing input taken from user && for array 2 llops are used for taking input and printing output
+	for (len=0;len < my_list.length;len++) //loop for printing input taken from user && for array 2 loops are used for taking input and printing output for More readable purpose or u can implement in one loop also
 	{
 		System.out.print(my_list[len] + "+"); // here use both array_name and index_name to print elements in array
 	}
