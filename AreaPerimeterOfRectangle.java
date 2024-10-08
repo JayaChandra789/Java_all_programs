fc560555fbfa1eecfaea3464db6fbb7cba4402fa
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-08 09:58:20 +0530

    Update AreaPerimeterOfRectangle.java

--- AreaPerimeterOfRectangle.java
+++ AreaPerimeterOfRectangle.java
@@ -1,32 +1,37 @@
 package nenu;
-import java.util.Scanner;
+import java.util.Scanner; //Importing Scanner class
 
-public class AreaPerimeterOfRectangle 
+public class AreaPerimeterOfRectangle //main class 
 {
-	public static int rectangle_area (int len,int width)
+	public static int rectangle_area (int len,int width) //Area Method with parameters len & width
 	{
-		int area = len * width;	
+		int area = len * width;	//Formula for area
 		return area	;
 	}
-	public static int rectangle_perimeter (int len,int width)
+	public static int rectangle_perimeter (int len,int width)//Perimeter Method with parameters len & width
 	{
-		int widt = 2*(len + width);	
-		return widt	;
+		int widt = 2*(len + width);	//Formula for Perimeter
+		return width	;
 	}
 	
-	public static void main(String[] args) 
+	public static void main(String[] args) //main method
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter len of triangle: ");
-		int len = sc.nextInt();
-		System.out.print("Enter Width of triangle: ");
-		int width = sc.nextInt();
-		
-		int rectangle_area =  rectangle_area ( len, width);
-		System.out.println("Area of reactangle is: " + rectangle_area);
-		
-		int rectangle_length =  rectangle_perimeter ( len, width);
-		System.out.print("Perimeter of reactangle is: " + rectangle_length);
+		Scanner sc = new Scanner(System.in); //Object for scanner class
+		   // Input for length
+        System.out.print("Enter length of rectangle: "); // Message
+        len = sc.nextInt(); // Assign input to static variable len
+
+        // Input for width
+        System.out.print("Enter width of rectangle: "); // Message
+        width = sc.nextInt(); // Assign input to static variable width
+
+        // Calculate and display area
+        System.out.println("Area of rectangle is: " + rectangle_area());
+
+        // Calculate and display perimeter
+        System.out.print("Perimeter of rectangle is: " + rectangle_perimeter());
+
+        sc.close(); // Close the scanner to prevent resource leaks
 				
 	}
 
