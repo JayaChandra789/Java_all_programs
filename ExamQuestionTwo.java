67c073808c683dfa5285e4fd25c22239b6f737f8
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-09 13:47:10 +0530

    Update ExamQuestionTwo.java

--- ExamQuestionTwo.java
+++ ExamQuestionTwo.java
@@ -2,40 +2,39 @@ package oopsExamples;
 import java.util.Scanner;
 
 // Custom Exception for invalid email format
-class InvalidEmailFormatException extends Exception 
+class InvalidEmailFormatException extends Exception //Custom Exception 
 {
-	public InvalidEmailFormatException(String message) 
+	public InvalidEmailFormatException(String message) //Constructor of class
 	{
-		super(message);
+		super(message); //super msg
 	}
 }
-public class ExamQuestionTwo
+public class ExamQuestionTwo //Main class
 {
 	public static void validateEmail(String email) throws InvalidEmailFormatException 
 	{  
-		String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"; 
 
-		if (!email.matches(emailRegex)) 
+		if (!email.matches(emailRegex)) //If condition not equals
 		{
 			throw new InvalidEmailFormatException("Invalid email format: " + email);
 		}
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args)  //Main method
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter an email address to validate: ");
-		String email = sc.nextLine();
+		Scanner sc = new Scanner(System.in); //Object for Scanner class
+		System.out.print("Enter an email address to validate: "); //prints message to user to enter E-mail
+		String email = sc.nextLine(); //Taking input
 
-		try 
+		try //Try block 
 		{
 			validateEmail(email);
 			System.out.println("Email is valid: " + email);
 		} 
-		catch (InvalidEmailFormatException e) 
+		catch (InvalidEmailFormatException e) //Object for Custom Excpetion
 		{
-
-			System.out.println(e.getMessage());
+			System.out.println(e.getMessage()); //Getting message from object
 		} 
 		finally 
 		{
