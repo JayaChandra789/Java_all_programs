5e4d481a425373240f0536f74eb18ce1731ac0f8
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-09 14:08:08 +0530

    Update PolymorphismEx.java

--- PolymorphismEx.java
+++ PolymorphismEx.java
@@ -8,19 +8,19 @@ class VoulmeCalculator
 	   int volume = length * length * length;   
 	   System.out.println("The volume of cube is: "+ volume);
 	}
-	public void calculateVolume(float width,int len ,float height) 
+	public void calculateVolume(float width,int len ,float height) /*method With different parameters*/
 	{
 		
 		float volume = len * width * height;
 		System.out.println("The volume of cuboid is: " +volume );
 	}	
 }
-public class PolymorphismEx 
+public class PolymorphismEx //Main class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)  //Main method
 	{
-		VoulmeCalculator vcobj = new VoulmeCalculator();
-		vcobj.calculateVolume(3);
-		vcobj.calculateVolume(3.0f,4,5.5f);
+		VoulmeCalculator vcobj = new VoulmeCalculator(); //Creating  Object Base class
+		vcobj.calculateVolume(3); //Calling Methods With arguments Passing 
+		vcobj.calculateVolume(3.0f,4,5.5f); //Calling Methods With arguments Passing 
 	}
 }
