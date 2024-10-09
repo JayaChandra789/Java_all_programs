fd335d481b0fa45cdf0b02f2d66f12f4d3b228eb
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-09 13:57:47 +0530

    Update ExampleMethod.java

--- ExampleMethod.java
+++ ExampleMethod.java
@@ -1,21 +1,20 @@
 package nenu;
 import java.util.Scanner;
-public class ExampleMethod 
+public class ExampleMethod //Main class
 {	//Don't use colon here because starting function 
 	//And 
-	public static void addition(int num1 ,int num2) 
+	public static void addition(int num1 ,int num2) //Addition method
 	{
-		int sum = num1 + num2;
-		System.out.print("The addition of " + num1 + " & "+ num2 +" is "+sum);
+		int sum = num1 + num2; //Adding num1 , num2
+		System.out.print("The addition of " + num1 + " & "+ num2 +" is "+sum);  //Printing sum of 2 numbers
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args) //Main method
 	{	
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Object for Scanner Class
 		System.out.print("Enter number 1: ");
 		int num1 = sc.nextInt();
 		System.out.print("Enter number 2: ");
 		int num2 = sc.nextInt();  
-		//Use colon here to end function here
 		addition(num1,num2);
 	}
 }
